47e65fb1532d05e3000ee227f1c463d33df1eb45
xna <xna00@qq.com>
2024-07-05 10:44:52 +0800

    Save

--- web/vite.config.ts
+++ web/vite.config.ts
@@ -1,5 +1,5 @@
 import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react-swc";
+import react from "@vitejs/plugin-react";
 import { svgDts } from "./svg";
 import svgr from "vite-plugin-svgr";
 
