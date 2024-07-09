81b58fb0e8bad0c465ecf4bb827bc7abfe89e436
xna <xna00@qq.com>
2024-07-09 15:31:05 +0800

    Filter inner functions

--- server/index.ts
+++ server/index.ts
@@ -1,4 +1,13 @@
+type FilterInner<K> = K extends `_${string}` ? never : K;
+
+type FilterKey<T> = {
+  [K in FilterInner<keyof T>]: T[K] extends (...args: never[]) => unknown
+    ? T[K]
+    : FilterKey<T[K]>;
+};
+
 import * as api from "./api/index.js";
-export type Api = typeof api;
+export type Api = FilterKey<typeof api>;
+
 import * as cst from "./constants/index.js";
 export { cst };
